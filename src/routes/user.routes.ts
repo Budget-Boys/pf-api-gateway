5a6209ee600d3f2618433a37b73a4a44fae1d51b
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-31 11:51:57 -0300

    feat: add user routes for creating and retrieving users

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import * as userController from '../controllers/user.controller';
+
+const router = Router();
+
+router.post('/', userController.createUser);
+
+router.get('/:id', userController.getUserById);
+
+export default router;
