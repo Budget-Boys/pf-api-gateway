5e5878079206d02dc7250eb0133bfe4a50c0b8b9
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-31 20:39:47 -0300

    fix: correct login route path in user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,7 +4,8 @@ import * as userController from '../controllers/user.controller';
 const router = Router();
 
 router.post('/', userController.createUser);
-
 router.get('/:id', userController.getUserById);
+router.post('/register', userController.registerUser);
+router.post('login', userController.loginUser);
 
 export default router;
