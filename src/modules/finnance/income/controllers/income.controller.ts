9b3e2f6f809635e150859c807f0c6110e0aaa7b5
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-06-02 17:12:30 -0300

    feat: add IncomeController for handling income-related requests

---
+++ src/modules/finnance/income/controllers/income.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { IncomeService } from "../services/income.service";
+import { CreateIncomeResDTO } from "../dtos/create-income.res.dto";
+import { CreateIncomeReqDTO } from "../dtos/create-income.req.dto";
+
+@Controller('incomes')
+export class IncomeController{
+    constructor(private readonly service: IncomeService){ }
+
+    @Get(':id')
+    public async getIncomeById(@Param('id') id: string): Promise<CreateIncomeResDTO> {
+    return await this.service.proxyFindIncomeById(id);
+    }
+    
+    @Post()
+    public async createIncome(@Body() payload: CreateIncomeReqDTO): Promise<CreateIncomeResDTO>{
+    return await this.service.proxyCreateIncome(payload)
+    }
+
+}
