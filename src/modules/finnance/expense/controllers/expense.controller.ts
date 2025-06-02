31f9ca7929a221376fba3e75a95380dd94f07e69
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-06-02 20:20:32 -0300

    feat: add ExpensesController for managing expense operations

---
+++ src/modules/finnance/expense/controllers/expense.controller.ts
@@ -0,0 +1,21 @@
+
+import { ExpenseService } from "../services/expense.service";
+import { CreateExpenseResDTO } from "../dtos/create-extense.res.dto";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { IExpense } from "../interfaces/expense.interface";
+import { CreateExpenseReqDTO } from "../dtos/create-extense.req.dto";
+
+@Controller('expenses')
+export class ExpensesController{
+    constructor(private readonly service: ExpenseService){}
+
+    @Get(':id')
+    public async getExpenseById(@Param('id') id:string) : Promise<CreateExpenseResDTO>{
+        return await this.service.proxyFindExpenseById(id)
+    }
+
+    @Post()
+    public async createExpense(@Body() payload: CreateExpenseReqDTO): Promise<CreateExpenseReqDTO>{
+        return await this.service.proxyCreateExpense(payload)
+    }
+}
