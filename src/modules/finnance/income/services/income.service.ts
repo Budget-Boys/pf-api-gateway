9568941d9d4b31c758194214200f91136d3aa626
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-06-02 17:12:25 -0300

    feat: implement IncomeService for income management operations

---
+++ src/modules/finnance/income/services/income.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from 'src/shared/http/services/http.service';
+import { TCreateIncome } from '../types/income.type';
+import { IIncome } from '../interfaces/income.interfaces';
+
+
+const financeServiceURL = process.env.FINANCE_SERVICE_URL || 'http://localhost:9000'
+
+@Injectable()
+export class IncomeService {
+    constructor(private readonly httpService: HttpService){}
+
+    public async proxyCreateIncome(payload: TCreateIncome): Promise<IIncome>{
+        return await this.httpService.proxy<IIncome>(
+            `${financeServiceURL}/incomes`, 
+            'POST',
+            {},
+            payload
+        )
+    }
+
+    public async proxyFindIncomeById(id: string): Promise<IIncome>{
+        return await this.httpService.proxy<IIncome>(
+         `${financeServiceURL}/incomes/${id}`,
+         'GET',
+         {},
+         undefined
+        )
+    }
+}
