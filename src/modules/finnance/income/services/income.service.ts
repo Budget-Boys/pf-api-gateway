0da21e602edc0065544788744392677332cffdc7
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-06-04 20:15:45 -0300

    refactor: update finance service URL and improve code formatting in IncomeService

--- src/modules/finnance/income/services/income.service.ts
+++ src/modules/finnance/income/services/income.service.ts
@@ -3,28 +3,34 @@ import { HttpService } from 'src/shared/http/services/http.service';
 import { TCreateIncome } from '../types/income.type';
 import { IIncome } from '../interfaces/income.interfaces';
 
-
-const financeServiceURL = process.env.FINANCE_SERVICE_URL || 'http://localhost:9000'
+const financeServiceURL = process.env.FINANCE_SERVICE_URL || 'http://localhost:9001';
 
 @Injectable()
 export class IncomeService {
-    constructor(private readonly httpService: HttpService){}
+  constructor(private readonly httpService: HttpService) {}
 
-    public async proxyCreateIncome(payload: TCreateIncome): Promise<IIncome>{
-        return await this.httpService.proxy<IIncome>(
-            `${financeServiceURL}/incomes`, 
-            'POST',
-            {},
-            payload
-        )
-    }
+  public async proxyCreateIncome(payload: TCreateIncome): Promise<IIncome> {
+    return await this.httpService.proxy<IIncome>(
+      `${financeServiceURL}/incomes`,
+      'POST',
+      {},
+      payload,
+    );
+  }
 
-    public async proxyFindIncomeById(id: string): Promise<IIncome>{
-        return await this.httpService.proxy<IIncome>(
-         `${financeServiceURL}/incomes/${id}`,
-         'GET',
-         {},
-         undefined
-        )
-    }
+  public async proxyFindIncomeById(id: string): Promise<IIncome> {
+    return await this.httpService.proxy<IIncome>(
+      `${financeServiceURL}/incomes/${id}`,
+      'GET',
+      {},
+      undefined,
+    );
+  }
+
+  public async proxyFindAllIncomes(): Promise<IIncome[]> {
+    return await this.httpService.proxy<IIncome[]>(
+        `${financeServiceURL}/incomes`, 
+        'GET',
+         {});
+  }
 }
