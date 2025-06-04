4a79658cb710727f2ae44edbd66d12480a264fdd
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-06-04 20:14:59 -0300

    feat: create IncomeModule with IncomeController and IncomeService

---
+++ src/modules/finnance/income/income.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { IncomeService } from './services/income.service';
+import { IncomeController } from './controllers/income.controller';
+import { HttpModule } from 'src/shared/http/https.module';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [IncomeController],
+  providers: [IncomeService],
+  exports: [],
+})
+export class IncomeModule {}
