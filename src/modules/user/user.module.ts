cc43470997c1d5a3e31a20296467e8d3d48cfb1b
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-06-04 20:14:18 -0300

    feat: add HttpModule to UserModule imports for enhanced HTTP functionality

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
+import { HttpModule } from 'src/shared/http/https.module';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [],
